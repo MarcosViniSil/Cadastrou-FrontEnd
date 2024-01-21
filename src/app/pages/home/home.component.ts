effc6402f687133bd78a085a084d37fb46a325ea
MarcosViniSil <Marcossilv203@gmail.com>
2024-01-21 15:50:36 -0300

    footer page main created

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 import { IntroductionComponent } from '../../components/introduction/introduction.component';
 import { HowItWorksComponent } from '../../components/how-it-works/how-it-works.component';
+import { AboutComponent } from '../../components/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuBarComponent,IntroductionComponent,HowItWorksComponent],
+  imports: [MenuBarComponent,IntroductionComponent,HowItWorksComponent,AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
